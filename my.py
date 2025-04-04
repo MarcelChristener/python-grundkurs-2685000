53e82e582dc834e4082531bccde1ef40744cc485
MarcelChristener <145452652+MarcelChristener@users.noreply.github.com>
2025-04-04 06:12:42 +0000

    meine übungs Datei

---
+++ my.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Variablen speichern Werte
+name = "Alice"  # String
+alter = 25  # Integer
+
+# Wert einer Variable ändern
+alter = 26
+
+# Typumwandlung (Casting)
+alter_str = str(alter)  # Integer zu String
+
+# Werte ausgeben
+print("Name:", name)
+print("Alter:", alter_str)
